ed3220358de236e84246530654de0384ea4e4026
DungxLas <85223326+DungxLas@users.noreply.github.com>
2023-08-02 21:59:18 +0700

    02/08/2023

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -5,19 +5,30 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.numberInput}
+        keyboardType="number-pad"
+        maxLength={2}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
+    alignItems: 'center',
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: '#72063c',
+    backgroundColor: '#4e0329',
     borderRadius: 8,
     elevation: 8,
     shadowColor: 'black',
@@ -25,6 +36,18 @@ const styles = StyleSheet.create({
     shadowRadius: 6,
     shadowOpacity: 0.25,
   },
+  numberInput: {
+    height: 58,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    textAlign: 'center',
+  },
+  buttonsContainer: {flexDirection: 'row'},
+  buttonContainer: {flex: 1},
 });
 
 export default StartGameScreen;
